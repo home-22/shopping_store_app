6053da3c098d5f86813e7f5108d746c7454f885e
home-22 <armin.jasarevic.25@gmail.com>
2022-08-31 23:24:13 +0200

    shopping_store_app

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -10,22 +11,64 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: GestureDetector(
-          child: ClipRRect(
-        borderRadius: BorderRadius.circular(35),
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
         child: Column(
           children: [
-            Expanded(
-                child: SizedBox(
-              width: double.infinity,
-              child: Image.asset(''),
-            ))
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.redAccent.withOpacity(0.5),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.sort,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.redAccent.withOpacity(0.5),
+                            blurRadius: 5,
+                            spreadRadius: 1,
+                          ),
+                        ],
+                      ),
+                      child: Badge(
+                        badgeColor: Colors.red,
+                        padding: EdgeInsets.all(6),
+                        badgeContent: Text(
+                          '4',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        child: Icon(
+                          Icons.notifications,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      )),
+                ],
+              ),
+            )
           ],
         ),
       )),
